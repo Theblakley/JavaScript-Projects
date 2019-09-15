fe54f15a8ab803bf55973d8012e55b509fc3be38
Theblakley <53923778+Theblakley@users.noreply.github.com>
2019-09-14 17:16:13 -0700

    Add files via upload

--- Basic JavaScripts Projects/JS/Basic_Javascript_1.js
+++ Basic JavaScripts Projects/JS/Basic_Javascript_1.js
@@ -1,3 +1,3 @@
-var Wife= "Emilie", Brother= "Alex", Dad="Leo", Mom= "Beth", Other_Bro="Brandon";
+var Wife= "", Brother= "Alex", Dad="Leo", Mom= "Beth", Other_Bro="Brandon";
 var Wife = Wife.fontcolor("turquoise");
 document.write(Brother);
